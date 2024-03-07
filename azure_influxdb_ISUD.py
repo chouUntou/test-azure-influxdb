cc5d5b87da48479a57a28186f59b94af85491af2
chouUntou <40907858+chouUntou@users.noreply.github.com>
2024-03-07 16:01:03 +0900

    Update azure_influxdb_ISUD.py

--- azure_influxdb_ISUD.py
+++ azure_influxdb_ISUD.py
@@ -4,25 +4,25 @@ from influxdb import InfluxDBClient
 # client = InfluxDBClient(host='74.226.208.84', port=8086, username=None, password=None, database='test_db')
 client = InfluxDBClient(host='4.216.59.33', port=8086, username='zhang', password='zhang', database='test_db')
 
-# データをクエリする
-#result = client.query('SELECT * FROM test_m LIMIT 5')
-#print("クエリ結果: {0}".format(result))
 
 # データを書き込む
 json_body = [
     {
-        "measurement": "test_m_1",
+        "measurement": "test_m",
         "tags": {
-            "tag1": "name",
-            "tag2": "age"
+            "tag1": "テスト用"
         },
         "fields": {
-            "field1": "張",
-            "field2": 18
+            "name": "張",
+            "age": 19
         }
     }
 ]
 client.write_points(json_body)
+
+# データをクエリする（読み込み）
+result = client.query('SELECT * FROM test_m LIMIT 5')
+print("クエリ結果: {0}".format(result))
 
 # 接続を閉じる
 client.close()
